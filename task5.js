fb0423801af423c0135a4bc54b90c3283ae8435e
Minea Micheal Nadakkalan <mineamicheal@gmail.com>
2026-07-24 07:47:43 +0530

    Delete task5.js

--- task5.js
+++
@@ -1,8 +0,0 @@
-
-let checkAge = 65;
- 
-if (checkAge >= 60) {
-  console.log(`Patient aged ${checkAge} is classified as a Senior Citizen patient.`);
-} else {
-  console.log(`Patient aged ${checkAge} is classified as a General patient.`);
-}
