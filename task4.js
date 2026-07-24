6d32db0d740caac3e09e614e9f61891831626a5c
Minea Micheal Nadakkalan <mineamicheal@gmail.com>
2026-07-24 07:47:55 +0530

    Delete task4.js

--- task4.js
+++
@@ -1,5 +0,0 @@
- let patient1Age = 45;
-let patient2Age = 30;
-let ageDifference = Math.abs(patient1Age - patient2Age);
- 
-console.log(`Age difference between Patient 1 (${patient1Age}) and Patient 2 (${patient2Age}) is: ${ageDifference} years`);
